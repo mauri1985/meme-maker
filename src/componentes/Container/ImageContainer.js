8396687c4158e0b43b5a417ce1156b6d0fcc8f09
mauricio.german.hernandez <mauricio.german.hernandez@fing.edu.uy>
2021-09-08 16:09:06 -0300

    mas cambios

--- src/componentes/Container/ImageContainer.js
+++ src/componentes/Container/ImageContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './ImageContainer.css';
+import Julio from '../../recursos/plantillas-meme/B6PM55DOKFC5TL5HMRCBEFYQ7A.jpg';
 
 class ImageContainer extends React.Component{
     constructor(props){
@@ -7,9 +8,10 @@ class ImageContainer extends React.Component{
     }
     render(){
         return (
-        <div className="imageContainer"> 
-            <h2>Plantilla {this.props.index}</h2>
-            <h3>{this.props.nombre}</h3>            
+        <div className="small"> 
+            <h4>Plantilla {this.props.index}</h4>
+            <p>{this.props.nombre}</p>            
+            <img src={Julio} className="ImageSRC" alt="logo" />
         </div>
         )}
 }
